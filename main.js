477c9b9662e1995de82b5d600171a8c75c7f25ac
Sam Mudick <smudick@gmail.com>
2020-07-21 20:01:36 -0500

    loop practice

--- main.js
+++ main.js
@@ -22,33 +22,33 @@
 // }
 // console.log(quoteFinder('w'))
 
-const employee = {
-  firstName: "Sam",
-  lastName: "Mudick",
-  role: "Student",
-  accountNumber: "1-2048287424",
-  isManager: false,
-  titles: ["Jr. Jr. Dev", "Sr. Student"],
-  accolades: {
-    title: "Employee of the Day",
-    dateEarned: "1/1/2020",
-    expires: "1/2/2020",
-    isCool: true,
-  },
-};
+// const employee = {
+//   firstName: "Sam",
+//   lastName: "Mudick",
+//   role: "Student",
+//   accountNumber: "1-2048287424",
+//   isManager: false,
+//   titles: ["Jr. Jr. Dev", "Sr. Student"],
+//   accolades: {
+//     title: "Employee of the Day",
+//     dateEarned: "1/1/2020",
+//     expires: "1/2/2020",
+//     isCool: true,
+//   },
+// };
 
-console.log(employee.firstName);
-const keyToCheck = "accolades";
+// console.log(employee.firstName);
+// const keyToCheck = "accolades";
 
-console.log(employee[keyToCheck]["isCool"]);
+// console.log(employee[keyToCheck]["isCool"]);
 
-const statusCheck = (person) => {
-  if (person.firstName[0] === "S") {
-    person.status = "vip";
-  } else {
-    person.status = "peasant";
-  }
-};
+// const statusCheck = (person) => {
+//   if (person.firstName[0] === "S") {
+//     person.status = "vip";
+//   } else {
+//     person.status = "peasant";
+//   }
+// };
 
-statusCheck(employee);
-console.log(employee);
+// statusCheck(employee);
+// console.log(employee);
